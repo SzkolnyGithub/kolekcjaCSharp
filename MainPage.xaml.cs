94da47bc73c5f56c5d515e8ddd9d8212422d9f98
GitMaster55 <144935449+SzkolnyGithub@users.noreply.github.com>
2023-11-10 14:56:27 +0100

    bardzo fajna sprawa

---
+++ MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace waznalekcja4cBadowski;
+using System.Collections.ObjectModel;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<Person> People { get; set; }
+
+    public MainPage()
+    {
+        InitializeComponent();
+        People = new ObservableCollection<Person>
+    {
+        new Person { Name = "Alice", Age = 30 },
+        new Person { Name = "Bob", Age = 25 },
+        new Person { Name = "Charlie", Age = 22 }
+    };
+        BindingContext = this;
+    }
+    private void Dodaj(object sender, EventArgs e)
+    {
+        if(Int32.Parse(wiek.Text) <= 0)
+        {
+            blad.Text = "ERROR: CANNOT INPUT NEGATIVE NUMBERS IN THE 'AGE' SECTION!";
+            imie.Text = "";
+            wiek.Text = "";
+            return;
+        } else
+        {
+            People.Add(new Person { Name = imie.Text, Age = Int32.Parse(wiek.Text) });
+            imie.Text = "";
+            wiek.Text = "";
+        }
+    }
+    private void Roznica(object sender, SelectedItemChangedEventArgs e)
+    {
+        string osoba = (e.SelectedItem as Person).Name;
+        int wiek = (e.SelectedItem as Person).Age;
+        if(wiek > 18)
+        {
+            DisplayAlert("Różnica wieku", "Różnica wieku osoby " + osoba + " i mojego to " + (wiek - 18), "OK");
+        } else
+        {
+            DisplayAlert("Różnica wieku", "Różnica wieku osoby " + osoba + " i mojego to " + (18 - wiek), "OK");
+        }
+    }
+
+}
+
